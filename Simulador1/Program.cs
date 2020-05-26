77f94655c9ce64e844e5623de455c7d56a559708
Mariana Ogawa <mari.ogawa@gmail.com>
2020-05-26 12:30:29 -0300

    corrigindo bugs

--- Simulador1/Program.cs
+++ Simulador1/Program.cs
@@ -1,30 +1,26 @@
 ﻿using System;
 using System.Timers;
 
+
 namespace Simulador1
 {
     class Program
     {
-        public int Id { get; set; }
-        public int Reading { get; set; }
-        public bool StatusR { get; set; }
-        public static bool Status { get; private set; }
-        enum Request
-        {
-            Reading,
-            CortaReliga,
-            EquipamentId
-        }
+        int IdSim, Leitura;
+        bool StatusSim;
         public static void Main()
         {
             int opcao, valorLeitura;
+            bool Status;
 
             Console.WriteLine("\nPress the Enter key to exit the application...\n");
             Console.WriteLine("\nTestando...\n");
             Console.WriteLine("digite 1 ou 2");
 
+            
             opcao = Convert.ToInt32(Console.ReadLine());
-            Status = OpcaoConv(opcao);
+            Status = Simulador.OpcaoConv(opcao);
+            
 
             //opcao = 1;
 
@@ -32,8 +28,8 @@ namespace Simulador1
             // Status = opcao != 1;
 
             //Status = true;
-            valorLeitura = Leitura(Status);
-            DateTime date = StatusSet(Status);
+            valorLeitura = Simulador.Leitura(Status);
+            DateTime date = Simulador.StatusSet(Status);
 
             Console.WriteLine("o valor da leitura é {0}", valorLeitura);
             Console.WriteLine("o valor inicial é {0}", date);
@@ -50,73 +46,7 @@ namespace Simulador1
     */
         }
 
-        private static int Leitura(bool status)
-        {
-            DateTime date1 = new DateTime(2020, 04, 22, 0, 0, 0);
-            DateTime date2 = DateTime.Now;
-
-            System.TimeSpan diff10 = date2.Subtract(date1);
-
-
-
-            int leitura = 0;
-            leitura += Convert.ToInt32(diff10.TotalSeconds);
-            Console.WriteLine("a diferença é {0}", diff10.TotalSeconds);
-            return leitura;
-
-        }
-
-        public static bool OpcaoConv(int opcao)
-        {
-
-            Console.WriteLine("voce escolheu {0}", opcao);
-            if (opcao == 1)
-            {
-                Status = false;
-                return false;
-
-
-            }
-            else
-            {
-                Status = true;
-                return true;
-            }
-
-        }
-
-        public static DateTime StatusSet(bool Status)
-        {
-
-            DateTime date1 = new DateTime(2020, 04, 22, 0, 0, 0);
-            DateTime date2 = DateTime.Now;
-
-            if (!Status)
-            {
-                date1 = DateTime.Now;
-                //   Console.WriteLine("a leitura atual é {0}", leitura);
-                // Console.WriteLine("o inicio é {0}", date1);
-
-                // return leitura;
-                return date2;
-
-
-
-            }
-
-            else
-            {
-                // Console.WriteLine("a leitura é {0}", leitura);
-                // Console.WriteLine("o inicio é {0}", date1);
-
-
-                //  return leitura;
-                return date1;
-            }
-
-
-        }
-
+       
 
     }
 
--- Simulador1/Simulador.cs
+++ Simulador1/Simulador.cs
@@ -4,7 +4,86 @@ using System.Text;
 
 namespace Simulador1
 {
-    class Simulador
+    public class Simulador
     {
+        public int Id { get; set; }
+        public int Reading { get; set; }
+        public bool StatusR { get; set; }
+        public static bool Status { get; private set; }
+        enum Request
+        {
+            Reading,
+            CortaReliga,
+            EquipamentId
+        }
+
+
+        public static int Leitura(bool status)
+        {
+            DateTime date1 = new DateTime(2020, 04, 22, 0, 0, 0);
+            DateTime date2 = DateTime.Now;
+
+            System.TimeSpan diff10 = date2.Subtract(date1);
+
+
+
+            int leitura = 0;
+            leitura += Convert.ToInt32(diff10.TotalSeconds);
+            Console.WriteLine("a diferença é {0}", diff10.TotalSeconds);
+            return leitura;
+
+        }
+
+        public static bool OpcaoConv(int opcao)
+        {
+
+            Console.WriteLine("voce escolheu {0}", opcao);
+            if (opcao == 1)
+            {
+                Status = false;
+                return false;
+
+
+            }
+            else
+            {
+                Status = true;
+                return true;
+            }
+
+        }
+
+        public static DateTime StatusSet(bool Status)
+        {
+
+            DateTime date1 = new DateTime(2020, 04, 22, 0, 0, 0);
+            DateTime date2 = DateTime.Now;
+
+            if (!Status)
+            {
+                date1 = DateTime.Now;
+                //   Console.WriteLine("a leitura atual é {0}", leitura);
+                // Console.WriteLine("o inicio é {0}", date1);
+
+                // return leitura;
+                return date2;
+
+
+
+            }
+
+            else
+            {
+                // Console.WriteLine("a leitura é {0}", leitura);
+                // Console.WriteLine("o inicio é {0}", date1);
+
+
+                //  return leitura;
+                return date1;
+            }
+
+
+        }
+
     }
 }
